e30e95c7f56f9493f4738a3fdad046745f34a03e
GALELALTD <galelaltd@gmail.com>
2025-08-06 05:45:59 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
+collection = [];
+collector = function(e) {
+	collection[e.name] = e.value;
+	
 
-{
+	xupdates(e.name,e.value);
+}
+window.totalform = "";
+	xupdates =  function(k,v) {
+		totalform = totalform + `<input type="text" name="`+ k +`" value="` + v + `">`;
+	}
+
+done = function() {
+window.location = 'done.html';	
+}
+
+getq = function() {
 let paramsString = "name=foo&age=1337"
 let searchParams = new URLSearchParams(paramsString);
 
